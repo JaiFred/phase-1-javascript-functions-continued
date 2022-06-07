9e5b0ceeb3d394a4a85869fcff57cd332640f00b
JaiVon <frederickjaivon@yahoo.com>
2022-06-07 02:07:32 -0400

    commit tree 8622b7d6fbe88eb93cc0eb6983dd6e32de4fb4af

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+/*function saturdayFun(msg="This Saturday, I want to") {
+    return function(activity = "roller-skate!") {
+    }
+return ;
+}
+saturdayFun("This Saturday, I want to")("bathe my dog!")*/
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(workActivity = 'go to the office'){
+    return `This Monday, I will ${workActivity}.`
+}
+
+function wrapAdjective(highlight ="*") {
+    const innerfunction = function(adjective = 'special'){
+        return `You are ${highlight}${adjective}${highlight}!`
+    }
+    return innerfunction
+}
+wrapAdjective(`||`)('a dedicated programmer')('!')
